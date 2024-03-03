713162b40861369b1b788bd6f432e05540a1abbb
meemee775 <160079351+meemee775@users.noreply.github.com>
2024-03-02 22:31:03 -0800

    first commit

---
+++ datatypes.dart
@@ -0,0 +1,33 @@
+void main() {
+//Declaring variables for numeric datatype(int)
+  int price = 200;
+  int tagnumber = 10;
+  //print values
+  print("price: $price and tagnumber: $tagnumber");
+
+  //Declaring variables for numeric datatype(double)
+  double weight = 6.2;
+  double height = 1.5;
+  //print values
+  print('height :$height and weight :$weight');
+
+//Declaring variables for String datatype
+  String name = "John";
+  String address = "Nigeria";
+  //print values
+  print("name :$name and address :$address");
+
+//Declaring variables for List datatype
+  List<String> statesinnigeria = ["Abia,Ebonyi,Enugu"];
+  //print values
+  print("statesinnigeria:$statesinnigeria");
+
+  //Declaring variables for Map datatype
+  Map<String, int> classaverage = {
+    'Basic1': 70,
+    'Basic2': 74,
+    'Basic3': 76,
+  };
+  //print values
+  print('classaverage: $classaverage');
+}
